c7eea0b7c4c594b0a3d645fb7b664c0bdc59d09a
Anubhav <tyagianubhav619@gmail.com>
2019-04-06 18:04:37 +0530

    Login portal complete

--- PlacementPortal/app/src/main/java/com/group6/placementportal/LoginPage.java
+++ PlacementPortal/app/src/main/java/com/group6/placementportal/LoginPage.java
@@ -1,5 +1,6 @@
 package com.group6.placementportal;
 
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,8 +9,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.net.PasswordAuthentication;
 
@@ -36,17 +40,38 @@ public class LoginPage extends AppCompatActivity {
         Webmail=findViewById(R.id.input_email);
         Password=findViewById(R.id.input_password);
         login_button=findViewById(R.id.btn_login);
-        login_button.setOnClickListener(new View.OnClickListener(){
+
+
+        login_button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View V){
+            public void onClick(View v) {
                 rollNo = Webmail.getText().toString();
                 password = Password.getText().toString();
-                check_password = Login_Details.child("Student").child(rollNo).child("Password").toString();
-                if(check_password == password){
-                    Toast.makeText(getBaseContext(),  "yes", Toast.LENGTH_LONG).show();
-                }else{
-                    Toast.makeText(getBaseContext(),  "no", Toast.LENGTH_LONG).show();
-                }
+
+                Login_Details= FirebaseDatabase.getInstance().getReference();
+                Login_Details = Login_Details.child("Student").child(rollNo).child("Password");
+
+
+                Login_Details.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        check_password = dataSnapshot.getValue(String.class);
+                        Toast.makeText(LoginPage.this, check_password, Toast.LENGTH_LONG).show();
+                        if(password.equals(check_password)){
+                            Toast.makeText(LoginPage.this, "Login successful", Toast.LENGTH_LONG).show();
+                        }
+                        else{
+                            Toast.makeText(LoginPage.this, "UNsuccessfull", Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        Toast.makeText(LoginPage.this, "UNsuccessfull", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+
             }
         });
 
