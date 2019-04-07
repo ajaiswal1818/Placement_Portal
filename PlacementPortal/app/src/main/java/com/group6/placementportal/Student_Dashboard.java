2a90274e68f9176318302231f9b71fa2ea2e7547
karlitowalker <ajaiswal9043@gmail.com>
2019-04-07 16:17:37 +0530

    nav_bar

---
+++ PlacementPortal/app/src/main/java/com/group6/placementportal/Student_Dashboard.java
@@ -0,0 +1,35 @@
+package com.group6.placementportal;
+
+import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class Student_Dashboard extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item){
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student__dashboard);
+
+
+
+
+    }
+}
+
+
