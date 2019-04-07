08f7f05b9d870af7b00cb905629773717f7ef2bb
AranyaAryaman <aryamanaranya@gmail.com>
2019-04-08 00:40:59 +0530

    Admin_home

---
+++ PlacementPortal/app/src/main/java/com/group6/placementportal/release_Notices.java
@@ -0,0 +1,13 @@
+package com.group6.placementportal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class release_Notices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_release__notices);
+    }
+}
