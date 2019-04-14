6c9a083ced9f4b8f814fcc1f43fa2cd18468afeb
Anubhav <tyagianubhav619@gmail.com>
2019-04-14 20:49:58 +0530

    Upload file Successful

--- PlacementPortal/app/src/main/java/com/group6/placementportal/Apply_For_Jobs.java
+++ PlacementPortal/app/src/main/java/com/group6/placementportal/Apply_For_Jobs.java
@@ -1,17 +1,16 @@
-package net.simplifiedlearning.firebasestorage;
+package com.group6.placementportal;
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,21 +23,20 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
-import com.group6.placementportal.R;
 
-public class Apply_For_Jobs extends AppCompatActivity implements View.OnClickListener {
+import java.security.PrivilegedAction;
 
-    //this is the pic pdf code used in file chooser
-    final static int PICK_PDF_CODE = 2342;
+public class Apply_For_Jobs extends AppCompatActivity {
+
+    private Uri pdfUri;
 
     //these are the views
-    TextView textViewStatus;
-    EditText editTextFilename;
-    ProgressBar progressBar;
+    private TextView fileName;
+    private ProgressDialog progressDialog;
 
     //the firebase objects for storage and database
-    StorageReference mStorageReference;
-    DatabaseReference mDatabaseReference;
+    private StorageReference mStorageReference;
+    private DatabaseReference mDatabaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,51 +46,66 @@ public class Apply_For_Jobs extends AppCompatActivity implements View.OnClickLis
 
         //getting firebase objects
         mStorageReference = FirebaseStorage.getInstance().getReference();
-        mDatabaseReference = FirebaseDatabase.getInstance().getReference(net.simplifiedlearning.firebasestorage.Constants.DATABASE_PATH_UPLOADS);
-
-        //getting the views
-        textViewStatus = (TextView) findViewById(R.id.textViewStatus);
-        editTextFilename = (EditText) findViewById(R.id.editTextFileName);
-        progressBar = (ProgressBar) findViewById(R.id.progressbar);
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference();
 
         //attaching listeners to views
-        findViewById(R.id.buttonUploadFile).setOnClickListener(this);
-        findViewById(R.id.textViewUploads).setOnClickListener(this);
+        fileName = findViewById(R.id.editTextFileName);
+        findViewById(R.id.buttonSelectFile).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getPDF();
+            }
+        });
+        findViewById(R.id.buttonUploadFIle).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(pdfUri!=null) {
+                    uploadFile(pdfUri);
+                }
+                else{
+                    Toast.makeText(Apply_For_Jobs.this,"Select a File",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     //this function will get the pdf from the storage
     private void getPDF() {
-        //for greater than lolipop versions we need the permissions asked on runtime
         //so if the permission is not available user will go to the screen to allow storage permission
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && ContextCompat.checkSelfPermission(this,
-                Manifest.permission.READ_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
-                    Uri.parse("package:" + getPackageName()));
-            startActivity(intent);
-            return;
+        if(ContextCompat.checkSelfPermission(Apply_For_Jobs.this,Manifest.permission.READ_EXTERNAL_STORAGE)==PackageManager.PERMISSION_GRANTED){
+            selectPDF();
         }
+        else{
+            ActivityCompat.requestPermissions(Apply_For_Jobs.this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},9);
+        }
+    }
 
-        //creating an intent for file chooser
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode==9 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+            selectPDF();
+        }
+        else{
+            Toast.makeText(Apply_For_Jobs.this,"Permission Denied",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void selectPDF() {
         Intent intent = new Intent();
         intent.setType("application/pdf");
         intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_PDF_CODE);
+        startActivityForResult(intent,86);
     }
 
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        //when the user choses the file
-        if (requestCode == PICK_PDF_CODE && resultCode == RESULT_OK && data != null && data.getData() != null) {
-            //if a file is selected
-            if (data.getData() != null) {
-                //uploading the file
-                uploadFile(data.getData());
-            }else{
-                Toast.makeText(this, "No file chosen", Toast.LENGTH_SHORT).show();
-            }
+        if(requestCode==86 && resultCode==RESULT_OK && data!=null){
+            pdfUri = data.getData();
+            fileName.setText(data.getData().getLastPathSegment());
+        }
+        else{
+            Toast.makeText(Apply_For_Jobs.this,"Select a file",Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -101,46 +114,40 @@ public class Apply_For_Jobs extends AppCompatActivity implements View.OnClickLis
     //the code is same as the previous tutorial
     //so we are not explaining it
     private void uploadFile(Uri data) {
-        progressBar.setVisibility(View.VISIBLE);
-        StorageReference sRef = mStorageReference.child(net.simplifiedlearning.firebasestorage.Constants.STORAGE_PATH_UPLOADS + System.currentTimeMillis() + ".pdf");
-        sRef.putFile(data)
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progressDialog.setTitle("Uploading...");
+        progressDialog.setProgress(0);
+        progressDialog.show();
+
+        final StorageReference ref = mStorageReference.child("Uploads").child("1");
+        ref.putFile(data)
                 .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @SuppressWarnings("VisibleForTests")
                     @Override
                     public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        progressBar.setVisibility(View.GONE);
-                        textViewStatus.setText("File Uploaded Successfully");
-
-                        net.simplifiedlearning.firebasestorage.Upload upload = new net.simplifiedlearning.firebasestorage.Upload(editTextFilename.getText().toString(), taskSnapshot.getMetadata().getReference().getDownloadUrl().toString());
-                        mDatabaseReference.child(mDatabaseReference.push().getKey()).setValue(upload);
+                        ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                progressDialog.hide();
+                                String upload = uri.toString();
+                                mDatabaseReference.child("Upload").setValue(upload);
+                                Toast.makeText(Apply_For_Jobs.this,"File Upload Successful",Toast.LENGTH_SHORT).show();
+                            }
+                        });
                     }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception exception) {
-                        Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_LONG).show();
-                    }
-                })
-                .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-                    @SuppressWarnings("VisibleForTests")
-                    @Override
-                    public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
-                        double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
-                        textViewStatus.setText((int) progress + "% Uploading...");
-                    }
-                });
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                progressDialog.hide();
+                Toast.makeText(getApplicationContext(), "File Upload Failed", Toast.LENGTH_LONG).show();
+            }
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
 
-    }
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.buttonUploadFile:
-                getPDF();
-                break;
-            case R.id.textViewUploads:
-                startActivity(new Intent(this, ViewUploadsActivity.class));
-                break;
-        }
+                int progress = (int) (100*taskSnapshot.getBytesTransferred()/taskSnapshot.getTotalByteCount());
+                progressDialog.setProgress(progress);
+            }
+        });
     }
 }
