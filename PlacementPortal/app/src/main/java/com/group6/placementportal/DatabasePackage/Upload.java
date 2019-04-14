30b45537cc4b40720fc2956ca8171b6464204bc0
Vakul Gupta <vakulgupta101@gmail.com>
2019-04-14 17:23:33 +0530

    Notifications started

--- PlacementPortal/app/src/main/java/com/group6/placementportal/DatabasePackage/Upload.java
+++ PlacementPortal/app/src/main/java/com/group6/placementportal/DatabasePackage/Upload.java
@@ -1,4 +1,28 @@
-package com.group6.placementportal.DatabasePackage;
+package net.simplifiedlearning.firebasestorage;
 
+/**
+ * Created by Belal on 8/25/2017.
+ */
 public class Upload {
+
+    public String name;
+    public String url;
+
+    // Default constructor required for calls to
+    // DataSnapshot.getValue(User.class)
+    public Upload() {
+    }
+
+    public Upload(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
 }
