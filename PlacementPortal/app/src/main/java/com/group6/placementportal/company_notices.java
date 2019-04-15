a3e9a23b296ad95617b9f5688e3091bcfb44d755
Utkarsh Mishra <utkarshm065@gmail.com>
2019-04-15 11:17:20 +0000

    company side of notices

--- PlacementPortal/app/src/main/java/com/group6/placementportal/company_notices.java
+++ PlacementPortal/app/src/main/java/com/group6/placementportal/company_notices.java
@@ -4,15 +4,38 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.util.ArrayList;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
 public class company_notices extends AppCompatActivity {
+    private CardArrayAdapter cardArrayAdapter;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_company_notices);
+
+        listView = (ListView) findViewById(R.id.card_listView);
+
+        listView.addHeaderView(new View(this));
+        listView.addFooterView(new View(this));
+
+        cardArrayAdapter = new CardArrayAdapter(getApplicationContext(), R.layout.list_item_card);
+
+        for (int i = 0; i < 10; i++) {
+            Card card = new Card("Card " + (i+1) + " Line 1", "Card " + (i+1) + " Line 2");
+            cardArrayAdapter.add(card);
+        }
+        listView.setAdapter(cardArrayAdapter);
+
+
 
     }
 
