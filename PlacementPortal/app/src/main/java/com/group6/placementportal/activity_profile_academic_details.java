412858034253b1e3c16c81b2a89561ea3ee6978c
rishipathak6 <radhakpathak5@gmail.com>
2019-04-17 17:39:54 +0530

    Expandable list

--- PlacementPortal/app/src/main/java/com/group6/placementportal/activity_profile_academic_details.java
+++ PlacementPortal/app/src/main/java/com/group6/placementportal/activity_profile_academic_details.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 public class activity_profile_academic_details extends AppCompatActivity {
 
-    private ExpandableListView listView;
+    private ExpandableListView listView,listView2;
     private ExpandableListAdapter listAdapter;
     private List<String> listDataHeader;
     private HashMap<String,List<String>> listHashMap;
@@ -22,9 +22,36 @@ public class activity_profile_academic_details extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_academic_details);
         listView = findViewById(R.id.lvExp);
+        listView2 = findViewById(R.id.pdExp);
         initData();
         listAdapter = new com.group6.placementportal.ExpandableListAdapter(this,listDataHeader,listHashMap);
         listView.setAdapter(listAdapter);
+        initData2();
+        listAdapter = new com.group6.placementportal.ExpandableListAdapter(this,listDataHeader,listHashMap);
+        listView2.setAdapter(listAdapter);
+
+    }
+
+    private void initData2() {
+        listDataHeader = new ArrayList<>();
+        listHashMap = new HashMap<>();
+
+        listDataHeader.add("Personal Details");
+
+        List<String> PersonalDetails = new ArrayList<>();
+        PersonalDetails.add("Name");
+        PersonalDetails.add("Father's Name");
+        PersonalDetails.add("Date of Birth");
+        PersonalDetails.add("Gender");
+        PersonalDetails.add("Category");
+        PersonalDetails.add("Religion");
+        PersonalDetails.add("State belongs to");
+        PersonalDetails.add("Address");
+        PersonalDetails.add("Mobile Number");
+        PersonalDetails.add("Phone Number");
+        PersonalDetails.add("Email Id");
+
+        listHashMap.put(listDataHeader.get(0),PersonalDetails);
 
     }
 
@@ -37,16 +64,34 @@ public class activity_profile_academic_details extends AppCompatActivity {
         listDataHeader.add("Graduation");
 
         List<String> Secondary = new ArrayList<>();
-        Secondary.add("Username");
-        Secondary.add("Username");
+        Secondary.add("Percentage");
+        Secondary.add("Year of Passing");
+        Secondary.add("Board");
 
         List<String> HigherSecondary = new ArrayList<>();
-        HigherSecondary.add("Username");
-        HigherSecondary.add("Username");
+        HigherSecondary.add("Percentage");
+        HigherSecondary.add("Year of Passing");
+        HigherSecondary.add("Board");
 
         List<String> Graduation = new ArrayList<>();
-        Graduation.add("Username");
-        Graduation.add("Username");
+        Graduation.add("Course Name");
+        Graduation.add("University Board");
+        Graduation.add("Semester 1 CPI");
+        Graduation.add("Date of Passing");
+        Graduation.add("Semester 2 CPI");
+        Graduation.add("Date of Passing");
+        Graduation.add("Semester 3 CPI");
+        Graduation.add("Date of Passing");
+        Graduation.add("Semester 4 CPI");
+        Graduation.add("Date of Passing");
+        Graduation.add("Semester 5 CPI");
+        Graduation.add("Date of Passing");
+        Graduation.add("Semester 6 CPI");
+        Graduation.add("Date of Passing");
+        Graduation.add("Semester 7 CPI");
+        Graduation.add("Date of Passing");
+        Graduation.add("Semester 8 CPI");
+        Graduation.add("Date of Passing");
 
         listHashMap.put(listDataHeader.get(0),Secondary);
         listHashMap.put(listDataHeader.get(1),HigherSecondary);
