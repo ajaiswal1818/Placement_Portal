94fc82a8da7d531c6e524ee51849745377d934c4
Vakul Gupta <vakulgupta101@gmail.com>
2019-04-18 02:14:14 +0530

    Added code for jrf approve and disapprove BUT NOT TESTED YET

--- PlacementPortal/app/src/main/java/com/group6/placementportal/JRF_Approval_Profile.java
+++ PlacementPortal/app/src/main/java/com/group6/placementportal/JRF_Approval_Profile.java
@@ -1,13 +1,23 @@
 package com.group6.placementportal;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.group6.placementportal.DatabasePackage.Notifications;
+import com.group6.placementportal.DatabasePackage.Student;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -19,6 +29,13 @@ public class JRF_Approval_Profile extends AppCompatActivity {
     private ExpandableListAdapter listAdapter;
     private List<String> listDataHeader;
     private HashMap<String,List<String>> listHashMap;
+    private DatabaseReference mDatabaseReference;
+    private DatabaseReference mDatabaseReference2;
+    private Button disapprove;
+    private Button approve;
+    private Notifications notif;
+    private Student user;
+    private String list_of_notif;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,9 +52,14 @@ public class JRF_Approval_Profile extends AppCompatActivity {
             }
         });
 
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference();
+        mDatabaseReference2 = FirebaseDatabase.getInstance().getReference();
         listView = findViewById(R.id.lv2Exp);
         listView2 = findViewById(R.id.lvExp);
         listView3 = findViewById(R.id.pdExp);
+        disapprove=findViewById(R.id.btn_disapprove);
+        approve=findViewById(R.id.btn_approve);
+
         initData();
         listAdapter = new com.group6.placementportal.ExpandableListAdapter(this,listDataHeader,listHashMap);
         listView.setAdapter(listAdapter);
@@ -47,6 +69,61 @@ public class JRF_Approval_Profile extends AppCompatActivity {
         initData3();
         listAdapter = new com.group6.placementportal.ExpandableListAdapter(this,listDataHeader,listHashMap);
         listView3.setAdapter(listAdapter);
+
+
+        disapprove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mDatabaseReference2 = mDatabaseReference2.child("Student").child("vakul170101076");
+                mDatabaseReference2.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        list_of_notif = dataSnapshot.child("List_of_Notification_IDs").getValue(String.class);
+                        list_of_notif+=",";
+                        list_of_notif+=(notif.getNotification_ID());
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+                mDatabaseReference2.child("List_of_Notification_IDs").setValue(list_of_notif);
+                mDatabaseReference.child("Notifications").child(notif.getNotification_ID()).child("Description").setValue("JRF application form rejected");
+                mDatabaseReference.child("Notifications").child(notif.getNotification_ID()).child("Read").setValue("False");
+                mDatabaseReference.child("Notifications").child(notif.getNotification_ID()).child("Subject").setValue("JRF APPLICATION REQUEST");
+                mDatabaseReference.child("Notifications").child(notif.getNotification_ID()).child("notification_ID").setValue(notif.getNotification_ID());
+
+            }
+        });
+
+        approve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDatabaseReference2 = mDatabaseReference2.child("Student").child("vakul170101076");
+                mDatabaseReference2.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        list_of_notif = dataSnapshot.child("List_of_Notification_IDs").getValue(String.class);
+                        list_of_notif+=",";
+                        list_of_notif+=(notif.getNotification_ID());
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+                mDatabaseReference2.child("List_of_Notification_IDs").setValue(list_of_notif);
+                mDatabaseReference.child("Notifications").child(notif.getNotification_ID()).child("Description").setValue("JRF application form approved");
+                mDatabaseReference.child("Notifications").child(notif.getNotification_ID()).child("Read").setValue("False");
+                mDatabaseReference.child("Notifications").child(notif.getNotification_ID()).child("Subject").setValue("JRF APPLICATION REQUEST");
+                mDatabaseReference.child("Notifications").child(notif.getNotification_ID()).child("notification_ID").setValue(notif.getNotification_ID());
+
+            }
+        });
+
     }
 
     private void initData3() {
@@ -120,12 +197,13 @@ public class JRF_Approval_Profile extends AppCompatActivity {
         listHashMap = new HashMap<>();
         listDataHeader.add("Registration Details");
         List<String> RegRDetails = new ArrayList<>();
-        RegRDetails.add("Name");
-        RegRDetails.add("Father's Name");
-        RegRDetails.add("Date of Birth");
-        RegRDetails.add("Gender");
-        RegRDetails.add("Category");
+        RegRDetails.add("Application No.");
+        RegRDetails.add("Programming Languages");
+        RegRDetails.add("Year and Type of Experiences");
+        RegRDetails.add("Applied For Project");
+        RegRDetails.add("Applied For Post");
         listHashMap.put(listDataHeader.get(0),RegRDetails);
     }
+
 
 }
