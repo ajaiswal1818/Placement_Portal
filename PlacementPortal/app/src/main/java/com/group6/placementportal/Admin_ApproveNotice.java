aa1b21b700cd914d7ba82f4accff72aa2e736a30
karl <ajaiswal9043@gmail.com>
2019-04-17 17:43:49 +0530

    images to notices added

--- PlacementPortal/app/src/main/java/com/group6/placementportal/Admin_ApproveNotice.java
+++ PlacementPortal/app/src/main/java/com/group6/placementportal/Admin_ApproveNotice.java
@@ -2,6 +2,9 @@ package com.group6.placementportal;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -10,14 +13,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class Admin_DashBoard extends AppCompatActivity
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.group6.placementportal.DatabasePackage.Notices;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Admin_ApproveNotice extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DatabaseReference reference;
+    private RecyclerView recyclerView;
+    private ArrayList<Notices> list;
+    private MyAdapter_Notices_FromCompany adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin__dash_board);
+        setContentView(R.layout.activity_admin__approve_notice);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -29,6 +48,46 @@ public class Admin_DashBoard extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+        recyclerView = findViewById(R.id.recyclerNoticesfromCompany);
+        recyclerView.setLayoutManager( new LinearLayoutManager(this));
+
+        reference = FirebaseDatabase.getInstance().getReference().child("Notices");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                list = new ArrayList<Notices>();
+                for(DataSnapshot dataSnapshot1: dataSnapshot.getChildren())
+                {
+                    if(dataSnapshot1.getKey().equals("dummy")) continue;
+                    Notices p = dataSnapshot1.getValue(Notices.class);
+                    list.add(p);
+                }
+                Collections.reverse(list);
+                adapter = new MyAdapter_Notices_FromCompany(Admin_ApproveNotice.this,list);
+                recyclerView.setAdapter(adapter);
+
+                adapter.setOnItemClickListener(new MyAdapter_Notices_FromCompany.OnItemClickListener() {
+                    @Override
+                    public void rejectnotice(int position) {
+                        list.remove(position);
+
+                    }
+
+                    @Override
+                    public void approvenotice(int position) {
+                        list.remove(position);
+                    }
+                });
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(Admin_ApproveNotice.this, "Opsss.... Something is wrong", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
--- PlacementPortal/app/src/main/java/com/group6/placementportal/DatabasePackage/Notices.java
+++ PlacementPortal/app/src/main/java/com/group6/placementportal/DatabasePackage/Notices.java
@@ -4,12 +4,21 @@ public class Notices {
 
     private String Topic;
     private String Content;
+    private String imageURL;
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
 
     public Notices(){
 
     }
 
-    public Notices(String id,String topic,String content){
+    public Notices(String topic,String content){
 
         this.Topic = topic;
         this.Content = content;
