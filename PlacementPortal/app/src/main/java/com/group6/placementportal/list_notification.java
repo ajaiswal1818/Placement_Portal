1badd54d10049362b358f00ae014e20b0a59bfb9
sunny <sunnykr1530@gmail.com>
2019-04-20 04:18:10 +0530

    BUGS REMOVED

--- PlacementPortal/app/src/main/java/com/group6/placementportal/list_notification.java
+++ PlacementPortal/app/src/main/java/com/group6/placementportal/list_notification.java
@@ -1,13 +1,11 @@
 package com.group6.placementportal;
 
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -15,8 +13,10 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.group6.placementportal.DatabasePackage.Notifications_Admin;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class list_notification extends AppCompatActivity {
 
@@ -25,63 +25,54 @@ public class list_notification extends AppCompatActivity {
     RecyclerView.LayoutManager mLayoutManager;
     DatabaseReference db;
     // ArrayList<notification_card> list = new ArrayList<>();
-    ArrayList<notification_card> cardList = new ArrayList<>();
+    ArrayList<Notifications_Admin> cardList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_notification);
 
-        if(isNetworkAvailable()==false){
-            Toast.makeText(list_notification.this,"NO INTERNET CONNECTION", Toast.LENGTH_LONG).show();
-            return;
-        }
-
 
 //            cardList.add(new notification_card("Line1"));
 //            cardList.add(new notification_card("Line2"));
 //            cardList.add(new notification_card("Line3"));
-            mrecyclerView = findViewById(R.id.recycler);
-            mrecyclerView.setHasFixedSize(true);
+        mrecyclerView = findViewById(R.id.recycler);
+        mrecyclerView.setHasFixedSize(true);
 
         mLayoutManager = new LinearLayoutManager(list_notification.this);
         mrecyclerView.setLayoutManager(mLayoutManager);
 
-            db = FirebaseDatabase.getInstance().getReference().child("notification_card");
-            db.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    if(dataSnapshot.exists())
-                    {
-                        for(DataSnapshot dataSnapshot1: dataSnapshot.getChildren()){
+        db = FirebaseDatabase.getInstance().getReference().child("Notifications_Admin");
+        db.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists())
+                {
+                    for(DataSnapshot dataSnapshot1: dataSnapshot.getChildren()){
 
-                            notification_card x = dataSnapshot1.getValue(notification_card.class);
-                            assert x != null;
-                            String subject = x.getText1();
-                            String destination = x.getText2();
-                            String pdflink = x.getText3();
-                            cardList.add(new notification_card(subject, destination,pdflink));
-                        }
+                        Notifications_Admin x = dataSnapshot1.getValue(Notifications_Admin.class);
+                        assert x != null;
+                        String subject = x.getSubject();
+                        String destination = x.getDescription();
+                        String pdflink = x.getPdflink();
+                        String notification_id = x.getNotification_ID();
 
-                        mAdapter = new notification_layout(list_notification.this,cardList);
-                        mrecyclerView.setAdapter(mAdapter);
-
+                        cardList.add(new Notifications_Admin(subject, destination,pdflink,notification_id));
                     }
 
+                    mAdapter = new notification_layout(list_notification.this,cardList);
+                    mrecyclerView.setAdapter(mAdapter);
+
                 }
 
-                @Override
-                public void onCancelled(@NonNull DatabaseError databaseError) {
-                    Toast.makeText(list_notification.this,"Something went Wrong",Toast.LENGTH_LONG).show();
-                }
-            });
+            }
 
-    }
-    private boolean isNetworkAvailable() {
-        ConnectivityManager connectivityManager
-                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
-        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(list_notification.this,"Something went Wrong",Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 
 }
